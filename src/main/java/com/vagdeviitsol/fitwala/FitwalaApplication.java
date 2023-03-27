dae980a363a8818121ab2ca10e268127853d8856
vagdeviitsolutions18 <116752605+vagdeviitsolutions18@users.noreply.github.com>
2023-03-27 23:57:32 +0530

    Initial commit with working spring boot project

---
+++ src/main/java/com/vagdeviitsol/fitwala/FitwalaApplication.java
@@ -0,0 +1,13 @@
+package com.vagdeviitsol.fitwala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitwalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitwalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vagdeviitsol/fitwala/FitwalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagdeviitsol.fitwala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitwalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
