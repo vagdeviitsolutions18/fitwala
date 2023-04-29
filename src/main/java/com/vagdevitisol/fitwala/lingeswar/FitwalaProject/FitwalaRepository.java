cd4e59ffdf72ca8a98f051b8b93b4dc7b6fe3a08
vblre <vblre@Lingeswar_Reddy.bbrouter>
2023-04-28 23:32:03 -0700

    FitwalaProject UserCreate and Update Lingeswar

---
+++ src/main/java/com/vagdevitisol/fitwala/lingeswar/FitwalaProject/FitwalaRepository.java
@@ -0,0 +1,21 @@
+package com.vagdevitisol.fitwala.lingeswar.FitwalaProject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface FitwalaRepository extends JpaRepository<User,Long>{
+    
+//	public User saveUser(User user);
+//	public User updatUser(User user);
+	
+//	List<User> findByFistName(String fistname);
+//
+//	void deleteByFistName(String fistname);
+
+	
+	
+	
+	 
+
+
+}
