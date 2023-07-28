89389b116c7ed4e50fd055b83b330429c0d7d978
Gopala Krishna <krishyadav5500@gmail.com>
2023-07-28 09:23:50 +0530

    Payment model added

---
+++ src/main/java/com/vagdeviitsol/fitwala/model/Payment.java
@@ -0,0 +1,69 @@
+package com.vagdeviitsol.fitwala.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Long paymentId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "amount")
+    private Double amount;
+
+    @Column(name = "plan_type")
+    private Integer planType;
+
+    @Column(name = "status")
+    private String status;
+   
+    
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Integer getPlanType() {
+        return planType;
+    }
+
+    public void setPlanType(Integer planType) {
+        this.planType = planType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
+
+
